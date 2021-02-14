32dc7d287834c3a9320534b4fa48e12191f602f5
Dakshj105 <42886981+Dakshj105@users.noreply.github.com>
2021-02-14 11:46:48 +0530

    Update KSmallestElement.py

--- KSmallestElement.py
+++ KSmallestElement.py
@@ -1,7 +1,23 @@
 def KSmallestElement(li, k):
-    li.sort()
-    for i in range(k):
-        print(li[i], end= " ")
+    c = 0
+    a = max(li)
+    li1 = li[:]
+    while(c < k):
+        c += 1
+        inde = -1
+        mini = a
+        for i in range(len(li1)):
+            if li1[i] <= mini:
+                mini = li1[i]
+                inde = i
+        li1[inde] = a
+    del li1
+    for i in li:
+        if i <= mini:
+            print(i, end = " ")
+
+        
+
 li = list(map(int, input().split()))
 k= int(input())
 KSmallestElement(li, k)
