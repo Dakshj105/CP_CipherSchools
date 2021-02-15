814f5590bf8b376edeed5f02a31c8696939884a6
Dakshj105 <42886981+Dakshj105@users.noreply.github.com>
2021-02-15 14:10:21 +0530

    Delete FindMissingNumber.py

--- FindMissingNumber.py
+++
@@ -1,9 +0,0 @@
-def FindMissingNumber(li, n):
-    s = sum(range(1, n+1))
-    for i in li:
-        s -= i
-    return s
-
-li = list(map(int, input().split()))
-n = int(input("total elements:- "))
-print(FindMissingNumber(li, n))
